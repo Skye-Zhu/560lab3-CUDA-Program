cdfea3f6599878c1dfb5bd8de24dfa6b86c764a9
Bensonl66 <129219423+Bensonl66@users.noreply.github.com>
2026-01-31 15:51:51 -0800

    Add files via upload
    New version

--- part2/call.py
+++ part2/call.py
@@ -5,32 +5,39 @@ import time
 lib = ctypes.cdll.LoadLibrary("./libmatrix.so")
 
 lib.gpu_convolution.argtypes = [
-    np.ctypeslib.ndpointer(dtype=np.uint32, ndim=1, flags="C"),
-    np.ctypeslib.ndpointer(dtype=np.uint32, ndim=1, flags="C"),
-    np.ctypeslib.ndpointer(dtype=np.uint32, ndim=1, flags="C"),
+    np.ctypeslib.ndpointer(dtype=np.uint32, ndim=1, flags="C_CONTIGUOUS"),  
+    np.ctypeslib.ndpointer(dtype=np.int32,  ndim=1, flags="C_CONTIGUOUS"),  
+    np.ctypeslib.ndpointer(dtype=np.uint32, ndim=1, flags="C_CONTIGUOUS"),  
     ctypes.c_int,
     ctypes.c_int
 ]
+lib.gpu_convolution.restype = None  
 
 M = 1024
 N = 3
 
 image = np.random.randint(0, 255, (M, M), dtype=np.uint32)
+
 kernel = np.array([
-    [1, 0, -1],
-    [1, 0, -1],
-    [1, 0, -1]
-], dtype=np.uint32)
+    [ 1,  0, -1],
+    [ 1,  0, -1],
+    [ 1,  0, -1]
+], dtype=np.int32)   
 
 output = np.zeros((M, M), dtype=np.uint32)
 
+img_buf = np.ascontiguousarray(image.ravel(), dtype=np.uint32)
+ker_buf = np.ascontiguousarray(kernel.ravel(), dtype=np.int32)
+out_buf = np.ascontiguousarray(output.ravel(), dtype=np.uint32)
+
+print("image:", img_buf.dtype, "contig:", img_buf.flags['C_CONTIGUOUS'])
+print("kernel:", ker_buf.dtype, "contig:", ker_buf.flags['C_CONTIGUOUS'])
+print("output:", out_buf.dtype, "contig:", out_buf.flags['C_CONTIGUOUS'])
+
 start = time.time()
-lib.gpu_convolution(
-    image.ravel(),
-    kernel.ravel(),
-    output.ravel(),
-    M, N
-)
+lib.gpu_convolution(img_buf, ker_buf, out_buf, M, N)
 end = time.time()
 
 print("CUDA convolution time:", end - start)
+
+output2d = out_buf.reshape(M, M)
